5ff88cf9621d60f38291259b6f557120f2a0fd0a
Nik <nikita.dovda.lg@gmail.com>
2025-12-14 19:43:31 +0200

    [*] set bridge and navmesh, add saves to built the bridge

--- Assets/Scripts/Buildings/BuildingController.cs
+++ Assets/Scripts/Buildings/BuildingController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DG.Tweening;
+using Extensions;
 using UnityEngine;
 
 public class BuildingController : MonoBehaviour
@@ -8,10 +9,14 @@ public class BuildingController : MonoBehaviour
     [SerializeField] private GameObject _obstakle;
     [SerializeField] private ConstructionZone _bridgeBuildZone;
 
+    private bool hasBuilt;
+
     private void Awake()
     {
+        hasBuilt = PlayerPrefsBool.GetBool(nameof(hasBuilt) + gameObject.name, false);
         bridgePrefab.SetActive(false);
         _bridgeBuildZone.OnComplete += BuildBridge;
+        if(hasBuilt) BuildBridge();
     }
 
     private void OnDestroy()
@@ -23,10 +28,13 @@ public class BuildingController : MonoBehaviour
     public void BuildBridge()
     {
        // Instantiate(bridgePrefab, buildPoint.position, buildPoint.rotation);
+       hasBuilt = true;
+       PlayerPrefsBool.SetBool(nameof(hasBuilt) + gameObject.name, hasBuilt);
        bridgePrefab.SetActive(true);
        _obstakle.SetActive(false);
        bridgePrefab.transform.localScale = Vector3.zero;
        bridgePrefab.transform.DOScale(1f, 0.25f).SetEase(Ease.OutBack);
+       _bridgeBuildZone.gameObject.SetActive(false);
        Debug.Log("Bridge built!");
     }
 }
