865373c87c92bea5c9a62692ab40b429b1db85b5
Nikita Dovda <zoomguru1@gmail.com>
2025-12-28 23:20:37 +0200

    [+] add scene loader

---
+++ Assets/Scripts/UI/SceneLoadButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+[RequireComponent(typeof(Button))]
+public class SceneLoadButton : MonoBehaviour
+{
+    [SerializeField] private string _sceneName;
+    [SerializeField] private bool _useIndex;
+    [SerializeField] private int _sceneIndex;
+
+    private ISceneLoader _sceneLoader;
+    private Button _button;
+
+    [Inject]
+    public void Construct(ISceneLoader sceneLoader)
+    {
+        _sceneLoader = sceneLoader;
+    }
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        if (_useIndex)
+        {
+            _sceneLoader.LoadScene(_sceneIndex);
+        }
+        else
+        {
+            _sceneLoader.LoadScene(_sceneName);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _button.onClick.RemoveListener(OnButtonClick);
+    }
+}
